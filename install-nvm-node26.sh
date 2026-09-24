30964a1e19db441d580c6284c7c0451212d86c87
Farhaz <farhaz@gmail.com>
2026-09-24 04:47:08 +0000

    Created and added package-lock.json file which is required

---
+++ install-nvm-node26.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install NVM and Node.js 26
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.8/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.8/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install 26
+nvm use 26
+npm install
+sudo rm -rf node_modules
+
+
+# This will generate a new package-lock.json file
